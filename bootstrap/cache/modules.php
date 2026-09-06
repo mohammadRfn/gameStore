eade748c145316e36ce8d78c16600e106adb0d66
Mohammad <mohammad.rfn98@gmail.com>
2026-09-06 21:31:32 +0330

    push

--- bootstrap/cache/modules.php
+++ bootstrap/cache/modules.php
@@ -1,11 +1,13 @@
 <?php return array (
   'providers' => 
   array (
-    0 => 'Modules\\Setting\\Providers\\SettingServiceProvider',
+    0 => 'Modules\\Profile\\Providers\\ProfileServiceProvider',
+    1 => 'Modules\\Setting\\Providers\\SettingServiceProvider',
   ),
   'eager' => 
   array (
-    0 => 'Modules\\Setting\\Providers\\SettingServiceProvider',
+    0 => 'Modules\\Profile\\Providers\\ProfileServiceProvider',
+    1 => 'Modules\\Setting\\Providers\\SettingServiceProvider',
   ),
   'deferred' => 
   array (
