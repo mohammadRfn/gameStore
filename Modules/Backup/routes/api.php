00c88b717d4f669f2358a6bee60882c67b436f20
Mohammad <mohammad.rfn98@gmail.com>
2026-09-15 13:44:29 +0330

    psda

--- Modules/Backup/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Backup\Http\Controllers\BackupController;
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('backups', BackupController::class)->names('backup');
-});
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,8 @@ use App\Http\Controllers\StatsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArchiveController;
 use Inertia\Inertia;
-use App\Http\Controllers\BackupController;
 
 Route::middleware('auth')->group(function () {
-
-    Route::post('/backup/pick-directory', [BackupController::class, 'pickDirectory']);
 
     Route::get('stats', [StatsController::class, 'index'])->name('stats.index');
     Route::get('stats/products', [StatsController::class, 'products'])->name('stats.products');
@@ -60,36 +57,6 @@ Route::middleware('auth')->group(function () {
         Route::delete('{archivedRecordId}', [ArchiveController::class, 'destroy'])
             ->whereNumber('archivedRecordId')
             ->name('destroy');
-    });
-
-    Route::prefix('backups')->name('backups.')->group(function () {
-
-        Route::get('panel', fn() => Inertia::render('Backup/Index'))->name('panel');
-        Route::get('overview', [BackupController::class, 'overview'])->name('overview');
-        Route::get('entities', [BackupController::class, 'entities'])->name('entities');
-
-        Route::prefix('export')->name('export.')->group(function () {
-            Route::post('validate-path', [BackupController::class, 'validateDestination'])->name('validate-path');
-            Route::post('/', [BackupController::class, 'export'])->name('run');
-            Route::post('database', [BackupController::class, 'exportDatabase'])->name('database');
-            Route::post('media', [BackupController::class, 'exportMedia'])->name('media');
-            Route::get('entity/{entityKey}.csv', [BackupController::class, 'downloadEntityCsv'])->name('entity-csv');
-        });
-
-        Route::prefix('import')->name('import.')->group(function () {
-            Route::post('inspect', [BackupController::class, 'inspect'])->name('inspect');
-            Route::post('dry-run', [BackupController::class, 'dryRun'])->name('dry-run');
-            Route::post('/', [BackupController::class, 'import'])->name('run');
-            Route::post('upload', [BackupController::class, 'upload'])->name('upload');
-        });
-
-        Route::get('/', [BackupController::class, 'index'])->name('index');
-        Route::get('settings', [BackupController::class, 'settings'])->name('settings.show');
-        Route::put('settings', [BackupController::class, 'updateSettings'])->name('settings.update');
-        Route::get('{runId}', [BackupController::class, 'show'])->whereNumber('runId')->name('show');
-        Route::get('{runId}/files', [BackupController::class, 'files'])->whereNumber('runId')->name('files');
-        Route::get('{runId}/log', [BackupController::class, 'downloadLog'])->whereNumber('runId')->name('log');
-        Route::delete('{runId}', [BackupController::class, 'destroy'])->whereNumber('runId')->name('destroy');
     });
 
     Route::resource('monthly-sales', MonthlySaleController::class)->only(['index', 'store']);
